b8314a0f0f91ee9ecfce4ecc1514d5dd0158c9da
winzer <hellochainl@gmail.com>
2023-04-04 13:49:02 -0400

    delete base.py by winzer

--- ChatGPT-3.5.py
+++ ChatGPT-3.5.py
@@ -2,15 +2,52 @@
 import openai
 import sys
 import prompt_toolkit
-from base import red
-from base import green
-from base import yellow
-from base import blue
-from base import reset
-from base import set_allow_tokens
-import base 
 
-openai.api_key = "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"
+
+#函数set_allow_tokens设置Allow_tokens
+Allow_tokens = 1000
+def set_allow_tokens():
+    def set_level_tokens(level_tokens):
+        global  Allow_tokens
+        Allow_tokens = level_tokens
+    def switcher(key):
+        func_dict = {
+            "1": lambda: set_level_tokens(100),
+            "2": lambda: set_level_tokens(500),
+            "3": lambda: set_level_tokens(1000),
+            "4": lambda: set_level_tokens(2000),
+            "5": lambda: set_level_tokens(3000),
+            "6": lambda: set_level_tokens(3500)
+        }
+        func = func_dict.get(key, None)
+        if func is not None:
+            func()
+        else:
+            print("无效的输入")
+            return
+    key = input("(1/2/3/4/5/6)：")
+    switcher(key)
+    return Allow_tokens
+
+
+
+#设置颜色
+red = "\033[1;31m"
+green = "\033[1;32m"
+yellow = "\033[1;33m"
+blue = "\033[1;34m"
+reset = "\033[0m"
+
+
+Question_length = 1000   #每个问题最大tokens数量
+total_tokens_last = 0  #上一次的总tokens
+messages=[]    #消息列表
+tokens_list=[]    #记录消息列表数量
+
+
+
+
+openai.api_key = "your API"
 
 
 #由用户设置记忆等级
@@ -36,16 +73,16 @@ input_message = {"role": "system", "content": "你是一个优秀的助手"}
 #输入合法性检查
 input_count = len(input_message["content"].encode('utf-8'))
 total_tokens = input_count
-if input_count > base.Question_length:
+if input_count > Question_length:
     print("输入太长")
     sys.exit()
 #拼接输入的场景
-base.messages.append(input_message)
+messages.append(input_message)
 
 #回答问题
 chatbot_response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
-  messages=base.messages
+  messages=messages
 )
 
 #输出答案
@@ -53,7 +90,7 @@ response_message = chatbot_response['choices'][0]['message']['content']
 
 #拼接输出的答案
 response_message = chatbot_response['choices'][0]['message']
-base.messages.append(response_message)
+messages.append(response_message)
 
 input_total_tokens = chatbot_response["usage"]["prompt_tokens"]
 total_tokens = chatbot_response["usage"]["total_tokens"]
@@ -64,8 +101,8 @@ output_tokens = chatbot_response["usage"]["completion_tokens"]
 total_tokens = int(str(total_tokens))
 
 
-base.tokens_list.append(input_tokens)
-base.tokens_list.append(output_tokens)
+tokens_list.append(input_tokens)
+tokens_list.append(output_tokens)
 
 
 while True:
@@ -76,11 +113,11 @@ while True:
     input_message = {"role": "user", "content": user_input}
     #输入合法性检查
     input_count = len(input_message["content"].encode('utf-8'))
-    if input_count > base.Question_length:
+    if input_count > Question_length:
         print("输入太长")
         sys.exit()
     #拼接输入的问题
-    base.messages.append(input_message)
+    messages.append(input_message)
 
     #拼接之后合法性检查
     #如果tokens过长
@@ -88,10 +125,10 @@ while True:
         total_tokens_last = total_tokens
         if total_tokens == 0:
             break
-        del base.messages[0]
-        del base.tokens_list[0]
+        del messages[0]
+        del tokens_list[0]
         total_tokens = 0
-        for num in base.tokens_list:
+        for num in tokens_list:
             total_tokens += num
 
     total_tokens_last = total_tokens
@@ -102,7 +139,7 @@ while True:
     #回答问题
     chatbot_response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
-      messages=base.messages
+      messages=messages
     )
 
     #输出答案
@@ -112,7 +149,7 @@ while True:
 
     #拼接输出的答案
     response_message = chatbot_response['choices'][0]['message']
-    base.messages.append(response_message)
+    messages.append(response_message)
 
     #计算tokens值
     total_tokens = chatbot_response["usage"]["total_tokens"]
@@ -123,8 +160,8 @@ while True:
     output_tokens = int(str(output_tokens)) 
     input_tokens = total_tokens - output_tokens - total_tokens_last   
     
-    base.tokens_list.append(input_tokens)
-    base.tokens_list.append(output_tokens)
+    tokens_list.append(input_tokens)
+    tokens_list.append(output_tokens)
     
 
 
